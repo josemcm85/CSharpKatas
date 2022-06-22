dc6937651d3c33c69ae341ae52b677065d090feb
Jose Chaves <josemcm85@gmail.com>
2022-06-21 22:32:19 -0600

    Setting Up Project

---
+++ CSharpKatas.Tests/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KatasCSharp;
+using Xunit;
+
+namespace CSharpKatas.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_SimpleValuesShouldCalculate()
+        {
+            //Arrange
+            double expected = 5;
+            //Act
+            double actual = Calculator.Add(3, 2);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
