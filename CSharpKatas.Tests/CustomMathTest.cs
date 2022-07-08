97ef3c47458948f5b0c44ba6296a69e8fc00743f
Jose Chaves <josemcm85@gmail.com>
2022-07-08 17:27:48 -0600

    Test Debugging CodeWars

---
+++ CSharpKatas.Tests/CustomMathTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using KatasCSharp;
+
+namespace CSharpKatas.Tests
+{
+    
+    public class CustomMathTest
+    {
+        [Fact]
+        public void ShouldMultiply()
+        {
+            Assert.Equal(2, CustomMath.multiply(2, 1));
+            Assert.Equal(8, CustomMath.multiply(2, 4));
+        }
+        
+    }
+}
