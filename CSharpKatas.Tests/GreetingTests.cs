836e0e636707c6fcd2e49ae3a610de4dfb4edae1
Jose Chaves <josemcm85@gmail.com>
2022-07-15 15:05:14 -0600

    GreetingTest

---
+++ CSharpKatas.Tests/GreetingTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KatasCSharp;
+using Xunit;
+
+namespace CSharpKatas.Tests
+{
+    public class GreetingTests
+    {
+        [Fact]
+        public void GreetingShouldReturnHi()
+        {
+            //Arrange
+            string name = "Ryan";
+
+            //Act
+            string actual = Greeting.Greet(name);
+
+            //Assert
+            Assert.Equal("Hello, Ryan how are you doing today?", actual);
+
+        }
+    }
+}
---
+++ KatasCSharp/Greeting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatasCSharp
+{
+    public class Greeting
+    {
+
+        public static string Greet(string name)
+        {
+            return "Hello, " + name + " how are you doing today?";
+        }
+
+    }
+}
